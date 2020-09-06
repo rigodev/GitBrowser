368091467b5592f4ea7e255b3f479fd8259b3dcb
Igor Shuvalov <shuvalov@morizo.ru>
2020-09-06 10:19:28 +0300

    Initial Commit

---
+++ GitBrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitBrowser
+//
+//  Created by Igor Shuvalov on 06.09.2020.
+//  Copyright © 2020 DevTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
